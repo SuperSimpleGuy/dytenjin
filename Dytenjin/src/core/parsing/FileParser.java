a3a2bb12fa6b711e3dab16a5aab66e956bed2a26
cjslep <cjslep@gmail.com>
2012-11-04 21:59:53 -0500

    Changed how exceptions were being handled. Will be fixed later (see issue 9)

--- Dytenjin/src/core/parsing/FileParser.java
+++ Dytenjin/src/core/parsing/FileParser.java
@@ -22,7 +22,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import core.Constants;
 import core.management.individual.IAspect;
 
 /**
@@ -33,13 +32,8 @@ public abstract class FileParser {
 
 	protected Scanner inFile;
 	
-	public FileParser(String f) {
-		try {
-			inFile = new Scanner(new File(f));
-		} catch (FileNotFoundException e) {
-			System.out.println(Constants.PARSER_ERROR_STRING);
-			System.exit(Constants.PARSER_ERROR_CODE);
-		}
+	public FileParser(String f) throws FileNotFoundException {
+		inFile = new Scanner(new File(f));
 	}
 	
 	public abstract String getDescription(IAspect a);
